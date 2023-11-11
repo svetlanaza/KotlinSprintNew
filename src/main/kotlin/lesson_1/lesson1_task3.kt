10c066d7b1f08174275544557bd5f9645ed7a630
svetlanaza <svetlanaz.dev@gmail.com>
2023-11-11 20:04:20 +0300

    KS-1-3 создание косммических данных

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,20 @@
+package lesson_1
+
+fun main() {
+    val yearOfFlight = 1961
+    var hour = 9
+    var minute = 7
+
+    println(yearOfFlight)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    println(minute)
+
+
+}
