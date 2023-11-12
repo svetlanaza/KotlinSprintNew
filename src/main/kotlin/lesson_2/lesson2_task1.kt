684d3bb39b19505ccd10b011657d62acf7edcc2f
svetlanaza <svetlanaz.dev@gmail.com>
2023-11-12 16:55:24 +0300

    KS-2-1 вычисление среднего арифметического

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+fun main() {
+    val numberOfStudents = 4
+    val mark1: Double = 3.0
+    val mark2 = 4
+    val mark3 = 3
+    val mark4 = 5
+    val markAverage = (mark1 + mark2 + mark3 + mark4) / numberOfStudents
+
+    println(markAverage)
+}
