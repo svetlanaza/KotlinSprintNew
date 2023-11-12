c49f755955a30600eb7032994fe63d070f201766
svetlanaza <svetlanaz.dev@gmail.com>
2023-11-12 17:49:38 +0300

    KS-2-2 создание программы для бухгалтерии

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -3,7 +3,7 @@ package lesson_2
 fun main() {
     val numberOfStudents = 4
     val mark1: Double = 3.0
-    val mark2 = 4
+    val mark2 = 6
     val mark3 = 3
     val mark4 = 5
     val markAverage = (mark1 + mark2 + mark3 + mark4) / numberOfStudents
