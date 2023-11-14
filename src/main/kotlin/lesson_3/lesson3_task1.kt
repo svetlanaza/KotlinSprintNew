e64a340d3ac588f78991b643b0f70d6501c061fc
svetlanaza <svetlanaz.dev@gmail.com>
2023-11-14 14:18:00 +0300

    KS-3-1 вывод приветствия на экран

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    val person = "Роман"
+    var greeting = "добрый день!"
+
+    println(person + ", " + greeting)
+
+    greeting = "добрый вечер!"
+
+    println("$person, $greeting")
+}
