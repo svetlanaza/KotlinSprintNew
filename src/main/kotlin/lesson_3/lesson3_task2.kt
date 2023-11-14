419ffc0e98a58df0f37539ea3538c6dfce253840
svetlanaza <svetlanaz.dev@gmail.com>
2023-11-14 16:08:08 +0300

    KS-3-2 создание записи об изменении личных данных

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    val lastName = "Сергеевна"
+    val surname = "Андреева"
+    val surnameNew = "Сидорова"
+    val age = 20
+    val agePlus = 22
+
+    println("В возрасте $age лет $name $lastName имеет фамилию - $surname.")
+    println("В возрасте $agePlus года $name $lastName имеет фамилию - $surnameNew")
+
+}
