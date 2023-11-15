0a809523a1c0cfcfdd42ec54f77d6f8e20ced41b
svetlanaza <svetlanaz.dev@gmail.com>
2023-11-15 18:05:23 +0300

    KS-3-5 создание программы для игры в шахматы

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,15 +1,31 @@
 package lesson_1
 
+//fun main() {
+//    val numberOfOrders = 75
+//    val notificationText = "Благодарим за покупку!"
+//
+//    println(numberOfOrders)
+//    println(notificationText)
+//
+//    var staff = 2000
+//    //println(staff)
+//
+//    staff = 2000 - 1
+//    println(staff)
+//}
 fun main() {
-    val numberOfOrders = 75
-    val notificationText = "Благодарим за покупку!"
 
-    println(numberOfOrders)
-    println(notificationText)
+    var countOfOrders: Int = 75
+    val gratitudeOrder: String = "Благодарим за покупку!"
 
-    var staff = 2000
-    //println(staff)
+    println("Кол-во заказов: $countOfOrders")
+    println(gratitudeOrder)
 
-    staff = 2000 - 1
-    println(staff)
+    var numbersOfWorkers: Int = 2000
+
+//  println("Кол-во работников: $numbersOfWorkers")
+
+    numbersOfWorkers = 1999
+
+    println("Кол-во работников: $numbersOfWorkers")
 }
