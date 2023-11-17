2ac7e5ee974e8925071928d1d29ae9483baa7dab
svetlanaza <svetlanaz.dev@gmail.com>
2023-11-17 15:54:28 +0300

    KS-4-3 создание программы для агррокомплекса исправления

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,18 +1,18 @@
 package lesson_4
 
-const val WEATHER = "sunny"
-const val TENT = "open"
+const val IS_SUNNY = true
+const val IS_TENT_OPEN = true
 const val HUMIDITY = 20
 const val SEASON = "winter"
 
 fun main() {
-    val todayWeather = "sunny"
-    val tentNaw = "open"
+    val isSunnyNow = true
+    val isTentNow = true
     val humidityAre = 20
-    var seasonNaw = "winter"
+    val seasonNow = "winter"
 
-    val isFavorableConditions = (todayWeather == WEATHER) && (tentNaw == TENT)
-                                && (humidityAre == HUMIDITY) && (seasonNaw != SEASON )
+    val isFavorableConditions = (isSunnyNow == IS_SUNNY) && (isTentNow == IS_TENT_OPEN)
+                                && (humidityAre == HUMIDITY) && (seasonNow != SEASON )
 
     println(isFavorableConditions)
 
