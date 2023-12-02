e82d729fc566cd772b8a401db7ca5ffabc9098e8
svetlanaza <svetlanaz.dev@gmail.com>
2023-12-02 11:31:47 +0300

    KS-1-3 запись космических данных исправленно на строки

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,16 +2,16 @@ package lesson_1
 
 fun main() {
     val yearOfFlight = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
 
     println(yearOfFlight)
-    println("0" + hour)
-    println("0" + minute)
+    println(hour)
+    println(minute)
 
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     print(hour)
     print(":")
