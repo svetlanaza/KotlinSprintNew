1287eb7ac62929b65ede9ea944bb7692514f9bad
svetlanaza <svetlanaz.dev@gmail.com>
2023-12-03 13:50:56 +0300

    KS-2-1 удаление ветки

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -5,7 +5,7 @@ fun main() {
     val mark1 = 3.0
     val mark2 = 6
     val mark3 = 3
-    val mark4 = 5
+    val mark4 = 7
     val markAverage = (mark1 + mark2 + mark3 + mark4) / numberOfStudents
 
     println(markAverage)
