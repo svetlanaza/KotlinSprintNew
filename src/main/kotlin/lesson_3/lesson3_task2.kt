8d552cbe50c48f58e702ac3ae8d23c0caa8c0adc
svetlanaza <svetlanaz.dev@gmail.com>
2023-12-04 14:09:03 +0300

    KS-3-2 изменение записи в гос приложении

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -3,12 +3,15 @@ package lesson_3
 fun main() {
     val name = "Татьяна"
     val lastName = "Сергеевна"
-    val surname = "Андреева"
-    val surnameNew = "Сидорова"
-    val age = 20
-    val agePlus = 22
+    var surname = "Андреева"
+    var age = 20
 
     println("В возрасте $age лет $name $lastName имеет фамилию - $surname.")
-    println("В возрасте $agePlus года $name $lastName имеет фамилию - $surnameNew")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("В возрасте $age лет $name $lastName имеет фамилию - $surname.")
+
 
 }
