afc9ec9991c24bbad3c8170be2cf21e57b183b19
svetlanaza <svetlanaz.dev@gmail.com>
2023-12-04 14:44:03 +0300

    KS-3-3 программа длч изучения таблицы умножения, измененеия захардкоженых чисел

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -1,43 +1,21 @@
 package lesson_3
 
 fun main() {
-    var number = 7
+    val number = 7
     var counter = 1
-    val calculation1 = counter * number
-    val calculation2 = ++counter * number
-    val calculation3 = ++counter * number
-    val calculation4 = ++counter * number
-    val calculation5 = ++counter * number
-    val calculation6 = ++counter * number
-    val calculation7 = ++counter * number
-    val calculation8 = ++counter * number
-    val calculation9 = ++counter * number
 
-    println("""
-        $number * 1 = $calculation1
-        $number * 2 = $calculation2
-        $number * 3 = $calculation3
-        $number * 4 = $calculation4
-        $number * 5 = $calculation5
-        $number * 6 = $calculation6
-        $number * 7 = $calculation7
-        $number * 8 = $calculation8
-        $number * 9 = $calculation9
+    println(
+        """
+        $number * $counter = ${number * counter}
+        $number * ${++counter} = ${number * counter}
+        $number * ${++counter} = ${number * counter}
+        $number * ${++counter} = ${number * counter}
+        $number * ${++counter} = ${number * counter}
+        $number * ${++counter} = ${number * counter}
+        $number * ${++counter} = ${number * counter}
+        $number * ${++counter} = ${number * counter}
+        $number * ${++counter} = ${number * counter}
         
-    """.trimIndent())
-
-    number = 5
-
-    println("""
-        $number * 1 = $calculation1
-        $number * 2 = $calculation2
-        $number * 3 = $calculation3
-        $number * 4 = $calculation4
-        $number * 5 = $calculation5
-        $number * 6 = $calculation6
-        $number * 7 = $calculation7
-        $number * 8 = $calculation8
-        $number * 9 = $calculation9
-        
-    """.trimIndent())
+    """.trimIndent()
+    )
 }
