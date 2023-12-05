88e715fcbe618274238d13ddd77dee849e68b385
svetlanaza <svetlanaz.dev@gmail.com>
2023-12-05 12:30:35 +0300

    KS-3-5 создание программы для игры в шахматы продолжение, исправления

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -6,9 +6,9 @@ fun main() {
     val splitData = inputData.split("-", ";")
     val from = splitData[0]
     val where = splitData[1]
-    val turnNumber = splitData[2]
+    val numberTurn = splitData[2]
 
     println(from)
     println(where)
-    println(turnNumber)
+    println(numberTurn)
 }
