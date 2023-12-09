855e996cbd9aa3df6e837def595a4e1dbe94e5f1
svetlanaza <svetlanaz.dev@gmail.com>
2023-12-09 10:55:16 +0300

    KS-7-2 авторизация по коду из sms, исправлено на инициализацию вне цикла

--- src/main/kotlin/lesson_7/lesson7_task2.kt
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -2,11 +2,12 @@ package lesson_7
 
 fun main() {
     val code = (1000..9999).random()
+    var codeEnter: Int
 
     do {
         println("Ваш код авторизации N: $code")
         println("Введите код")
-        val codeEnter = readln().toInt()
+        codeEnter = readln().toInt()
 
     } while (codeEnter != code)
     println("Добро пожаловать!")
