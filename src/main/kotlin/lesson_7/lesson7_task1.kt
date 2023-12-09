ec120b5a6bf38550eeb988861298c4967bdcb170
svetlanaza <svetlanaz.dev@gmail.com>
2023-12-09 10:46:48 +0300

    KS-7-1 генерирование шестизначных паролей, исправлено на остаток деления

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -4,17 +4,15 @@ fun main() {
     val range1 = 1..9
     val range2 = 'a'..'z'
     var password = ""
-    var isChar = true
 
     for (i in 1..6) {
-        if (isChar) {
+        if (i % 2 == 0) {
             range1.random()
             password += range1.random()
         } else {
             range2.random()
             password += range2.random()
         }
-        isChar = !isChar
     }
     println(password)
 }
