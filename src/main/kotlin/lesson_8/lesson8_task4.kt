6fd9a476476677b76a7d7ab2c019bedac92cca4c
svetlanaza <svetlanaz.dev@gmail.com>
2024-03-07 14:59:40 +0300

    KS-8-4 замена ингредиента, исправления

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -1,34 +1,26 @@
 package lesson_8
 
 fun main() {
-    var arrayOfIngredients = arrayOf("апельсин", "имбирь", "сахар", "корица")
-
-    println(arrayOfIngredients[0])
-    println(arrayOfIngredients[1])
-    println(arrayOfIngredients[2])
-    println(arrayOfIngredients[3])
-    println()
+    val arrayOfIngredients = arrayOf("апельсин", "имбирь", "сахар", "корица")
+    println("В рецепте есть базовые ингредиенты:")
+    arrayOfIngredients.forEach {
+        println(it)
+    }
 
     println("Какой ингредиент вы хотите заменить?")
 
     val ingredient = readln()
     val index = arrayOfIngredients.indexOf(ingredient)
-
     if (index == -1) {
         println("Такого ингредиента в рецепте нет")
-        return
+    } else {
+        println("Какой ингредиент вы хотите добавить?")
     }
-    println("Какой ингредиент вы хотите добавить?")
-
     val ingredientNew = readln()
-    arrayOfIngredients.set(index, ingredientNew)
-    arrayOfIngredients.get(index)
+    arrayOfIngredients[index] = ingredientNew
 
     println("Ваш новый рецепт:")
-    println(arrayOfIngredients[0])
-    println(arrayOfIngredients[1])
-    println(arrayOfIngredients[2])
-    println(arrayOfIngredients[3])
-
-
+    arrayOfIngredients.forEach {
+        println(it)
+    }
 }
