41b60df683d5b200bda68439c61f9fb6abf45d96
svetlanaza <svetlanaz.dev@gmail.com>
2024-03-07 13:58:40 +0300

    KS-7-5 создание сложных паролей, убрала return

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -2,18 +2,35 @@ package lesson_7
 
 fun main() {
 
-    println("Сколько символов будет в вашем пароле?")
+    println("Сколько символов будет в вашем пароле (минимум 6)?")
 
-    val chars = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-    val numberSymbolMin = 6
+    val charRange = 'a'..'z'
+    val intRange = '0'..'9'
+    val charRangeRegister = 'A'..'Z'
+    val allChars = charRange + intRange + charRangeRegister
     val numberSymbolMax = readln().toInt()
-    val password = if (numberSymbolMax >= numberSymbolMin) {
-        chars.shuffled().take(numberSymbolMax).joinToString("")
+    val password = mutableListOf<Char>()
+
+    if (numberSymbolMax >= 6) {
+
+        password.add(charRange.random())
+        password.add(intRange.random())
+        password.add(charRangeRegister.random())
+
+        for (i in 4..numberSymbolMax) {
+            password.add(allChars.random())
+        }
+        password.shuffle()
+        println(password.joinToString(""))
     } else {
-        println("Недостататочно символов")
+        println("Не достаточно символов")
     }
-    println("Ваш пароль: $password")
 }
+
+
+
+
+
 
 
 
