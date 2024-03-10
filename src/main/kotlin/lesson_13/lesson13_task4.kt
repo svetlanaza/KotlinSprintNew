b80f49eaa1fb451b2f1af779e3d450d2ef84c1ef
svetlanaza <svetlanaz.dev@gmail.com>
2024-03-10 15:19:37 +0300

    KS-13-4 создание класса ктелефонной книги

--- src/main/kotlin/lesson_13/lesson13_task4.kt
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -1,9 +1,9 @@
 package lesson_13
 
 class ClientContact(
-    val name: String,
-    val phoneNumber: Long,
-    val company: String? = null
+    private val name: String,
+    private val phoneNumber: Long,
+    private val company: String? = null
 ) {
     fun printInfo() {
         println("Имя: $name\nНомер: $phoneNumber\nКомпания: ${company ?: "<не указано>"}")
@@ -13,10 +13,13 @@ class ClientContact(
 fun main() {
 
     val contacts = mutableListOf<ClientContact>()
-    println("Хотите добавить контакт?")
-    val onswer = readln()
 
-    while (onswer== "да") {
+    while (true) {
+        println("Хотите добавить контакт?")
+        val answer = readln()
+        if (answer == "нет") {
+            return
+        }
         print("Введите имя")
         val name = readln()
         if (name.isEmpty()) break
@@ -24,6 +27,7 @@ fun main() {
         print("Введите номер телефона")
         val phoneNumberInput = readln()
         val phoneNumber = phoneNumberInput.toLongOrNull()
+
         if (phoneNumber == null) {
             println("Неправильно введен номер")
             continue
@@ -32,7 +36,8 @@ fun main() {
         val company = readln().let { if (it.isEmpty()) null else it }
 
         contacts.add(ClientContact(name, phoneNumber, company))
+        contacts.forEach {
+            it.printInfo()
+        }
     }
-
-    contacts.forEach { it.printInfo() }
 }
