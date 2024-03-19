96ad4d59e5878cf7cd661e10bffa1acd9c227939
svetlanaza <svetlanaz.dev@gmail.com>
2024-03-19 17:09:04 +0300

    KS-11-4 создание классов для сущностей

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -14,42 +14,7 @@ class Ingredient(
 
 class Recipe(
     val title: String,
-    val cookingInstructions: String
+    val cookingInstructions: String,
+    val ingredients: List<Ingredient>,
+    val method: List<String>,
 )
-fun main() {
-    val category = Category(
-        1,
-        "Бургеры",
-        "gjk1.image",
-        "Рецепты всех популярных бургеров"
-    )
-    val recipe = Recipe(
-         "Бугрер с грибами и сыроом",
-        "Положите котлеты на нижнюю часть булочек" +
-                " и выложите сверху каждой котлеты кетчуп, " +
-                "если используете, и большую ложку с горкой грибной икры. " +
-                "Накройте верхушками булочек"
-    )
-    val ingredients = listOf(
-        Ingredient("творог", 350, "гр"),
-        Ingredient("яйцо", 2, "шт"),
-        Ingredient("мука", 6, "ст. ложка"),
-        Ingredient("подсолнечное масло", 5, "ст. ложка"),
-        Ingredient("сахар", 2, "ст. ложка")
-    )
-
-
-    println(category.id)
-    println(category.title)
-    println(category.imageLinc)
-    println(category.description)
-    println()
-    println(recipe.title)
-    for (ingredient in ingredients) {
-        println("${ingredient.title}: ${ingredient.quantity} ${ingredient.unitOfMeasure}")
-    }
-
-
-
-
-}
