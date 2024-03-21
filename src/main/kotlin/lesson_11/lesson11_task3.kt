47ad4227571e1f1a3726e152ac0b79b27f742e11
svetlanaza <svetlanaz.dev@gmail.com>
2024-03-21 17:19:59 +0300

    KS-11-3 социальная сеть, добавила реализацию

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -5,6 +5,7 @@ class Participant(
     val avatar: String,
     var status: String,
 )
+
 class Room(
     val cover: String,
     val title: String,
@@ -15,27 +16,26 @@ class Room(
     }
 
     fun updateParticipantStatus(nickname: String, newStatus: String) {
-
+        participants.find { it.nickname == nickname }?.status = newStatus
     }
 
     fun printParticipantInfo(nickname: String, status: String) {
-            println("Nickname: $nickname, Status: $status")
+        participants.forEach {
+            println("Участник: ${it.nickname}, Статус: ${it.status}")
         }
     }
-
+}
 
 fun main() {
+
     val room = Room(
         cover = "cover_image.jpg",
-        title = "Комната любителей Kotlin"
+        title = "Комната любителей Kotlin",
     )
+    println("Комната: ${room.title}")
+
     room.addParticipant(Participant(nickname = "User1", avatar = "avatar1.jpg", status = "разговаривает"))
     room.addParticipant(Participant(nickname = "User2", avatar = "avatar2.jpg", status = "микрофон выключен"))
     room.updateParticipantStatus(nickname = "User2", newStatus = "пользователь заглушен")
     room.printParticipantInfo(nickname = "User2", status = "пользователь заглушен")
-
-    println("Комната: ${room.title}")
-    room.participants.forEach {
-        println("Участник: ${it.nickname}, Статус: ${it.status}")
-    }
 }
