ce3b6af664d446b120b94df0f689ce7a31e37235
svetlanaza <svetlanaz.dev@gmail.com>
2024-03-21 16:08:40 +0300

    KS-8-4 изменила логику в if

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -11,16 +11,16 @@ fun main() {
 
     val ingredient = readln()
     val index = arrayOfIngredients.indexOf(ingredient)
-    if (index == -1) {
-        println("Такого ингредиента в рецепте нет")
-    } else {
-        println("Какой ингредиент вы хотите добавить?")
-    }
-    val ingredientNew = readln()
-    arrayOfIngredients[index] = ingredientNew
+    if (index != -1) {
+        println("Какой ингредиент вы хотите добавить")
+        val ingredientNew = readln()
+        arrayOfIngredients[index] = ingredientNew
 
-    println("Ваш новый рецепт:")
-    arrayOfIngredients.forEach {
-        println(it)
+        println("Ваш новый рецепт:")
+        arrayOfIngredients.forEach {
+            println(it)
+        }
+    } else {
+        println("Такого ингредиента в рецепте нет")
     }
 }
