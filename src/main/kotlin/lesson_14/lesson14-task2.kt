468b1b56ab1950a715dd5ceda22a8377055b2c6c
svetlanaza <svetlanaz.dev@gmail.com>
2024-03-26 13:36:44 +0300

    KS-14-2 создание иерархии классов и переопределение методов

--- src/main/kotlin/lesson_14/lesson14-task2.kt
+++ src/main/kotlin/lesson_14/lesson14-task2.kt
@@ -1,70 +1,73 @@
 package lesson_14
 
-open class Ship(
+open class Ship1(
     val name: String,
     val speed: Int,
     val capacity: Int,
     val tonnage: Int,
 ) {
     fun printData() {
-        println("Корабль $name имеет: " +
-                "скорость $speed, " +
-                "вместительность $capacity пассажиров, " +
-                "грузоподъемность $tonnage.")
+        println(
+            "Корабль $name имеет: " +
+                    "скорость $speed, " +
+                    "вместительность $capacity пассажиров, " +
+                    "грузоподъемность $tonnage."
+        )
+    }
+
+    open fun loadMethod() {
+        println("$name: выдвигает горизонтальный трап со шкафута")
     }
 }
 
-class Liner1(name: String,
-             speed: Int,
-             capacity: Int,
-             tonnage: Int,
-) : Ship(name, speed, capacity, tonnage) {
-
-    fun extendTheLadder() {
-        println("Метод погрузки: выдвинуть горизонтальный трап.")
-    }
-}
+class Liner1(
+    name: String,
+    speed: Int = 150,
+    capacity: Int = 2000,
+    tonnage: Int = 15000,
+) : Ship1(name, speed, capacity, tonnage)
 
 class Icebreaker1(
     name: String,
-    speed: Int,
-    capacity: Int,
-    tonnage: Int,
-) : Ship(name, speed, capacity, tonnage) {
+    speed: Int = 50,
+    capacity: Int = 100,
+    tonnage: Int = 10000,
+) : Ship1(name, speed, capacity, tonnage) {
 
     fun breakTheIce() {
         println("Может колоть лед.")
     }
 
-    fun openGateTheStern() {
-        println("Метод погрузки: открыть ворота на корме.")
+    override fun loadMethod() {
+        println("$name: открывает ворота со стороны кормы.")
     }
 }
 
-class Tanker1(name: String,
-              speed: Int,
-              capacity: Int,
-              tonnage: Int,
-) : Ship(name, speed, capacity, tonnage) {
+class Tanker1(
+    name: String,
+    speed: Int = 30,
+    capacity: Int = 150,
+    tonnage: Int = 30000,
+) : Ship1(name, speed, capacity, tonnage) {
 
-    fun activateLoadingCrane() {
-        println("Метод погрузки: активировать погрузочный кран.")
+    override fun loadMethod() {
+        println("$name: активирует погрузочный кран.")
     }
 }
 
 fun main() {
-    val liner = Liner1("Быстрый", 500, 3000, 2000)
-    val icebreaker = Icebreaker1("Ленин", 100, 50, 10000)
-    val tanker = Tanker1("Адмирал", 200, 100, 20000)
+    val liner = Liner1("Быстрый")
+    val icebreaker = Icebreaker1("Ленин")
+    val tanker = Tanker1("Адмирал")
 
     liner.printData()
-    liner.extendTheLadder()
+    liner.loadMethod()
     println()
     icebreaker.printData()
-    icebreaker.openGateTheStern()
+    icebreaker.loadMethod()
     icebreaker.breakTheIce()
     println()
     tanker.printData()
-    tanker.activateLoadingCrane()
-
+    tanker.loadMethod()
 }
+
