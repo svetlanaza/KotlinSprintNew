83ac252be9250af2e51d790ce3565ba2a0ece44e
svetlanaza <svetlanaz.dev@gmail.com>
2024-03-28 11:13:41 +0300

    KS-14-4 создание класса планет

--- src/main/kotlin/lesson_14/lesson14_task4.kt
+++ src/main/kotlin/lesson_14/lesson14_task4.kt
@@ -10,32 +10,36 @@ class Planet(
     category: String,
     val isAtmosphere: Boolean = false,
     val isPossibilityOfDisembarkation: Boolean = false,
-
+    val satellites: List<Satellite>,
     ) : HeavenlyBody(name, category) {
 
     fun printInfoPlanet() {
-        println("Небеснное тело $name относися к категории: $category;\n"+
-                "Наличие атмосферы - ${isAtmosphere};\n"+
-                "Возможнность высадки - ${isPossibilityOfDisembarkation};\n"+
-                "Имеет 2 спутника:")
+        println(
+            "Небеснное тело $name относися к категории: $category;\n" +
+                    "Наличие атмосферы - ${isAtmosphere};\n" +
+                    "Возможнность высадки - ${isPossibilityOfDisembarkation};\n" +
+                    "Имеет 2 спутника:"
 
+        )
+        satellites.forEach {
+            println(it.name)
     }
 }
 
 class Satellite(
     name: String,
-    category: String
+    category: String,
 ) : HeavenlyBody(name, category)
 
 fun main() {
-    val planet = Planet("Марс", "Планеты")
-    val satellites = listOf(
-        Satellite("Фобос", "Спутники"),
-        Satellite("Деймос", "Спутники")
-    )
+    val satellite1 = Satellite("Фобос", "Спутники")
+    val satellite2 = Satellite("Деймос", "Спутники")
+    val planet = Planet("Марс", "Планеты", false,
+        false, listOf(satellite1,satellite2))
+
+    
     planet.printInfoPlanet()
-    satellites.forEach {
-        println(it.name)
+
     }
 }
 
