c268611042cb05ed07bd089ae881ed5f225f115f
svetlanaza <svetlanaz.dev@gmail.com>
2024-04-16 16:31:30 +0300

    KS-17-3 создание класса секретная папка

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -2,15 +2,15 @@ package lesson_17
 
 class Folder(name: String, numberOfFiles: Int, private val isSecret: Boolean) {
     val folderName = name
-        get() = if (isSecret == true) "скрытая папка" else field
+        get() = if (isSecret) "скрытая папка" else field
 
     val folderFileCount = numberOfFiles
-        get() = if (isSecret == true) 0 else field
+        get() = if (isSecret) 0 else field
 }
 
 fun main() {
 
-    val secretFolder = Folder("Секретные материалы", 5, true)
+    val secretFolder = Folder("Секретные материалы", 5, false)
 
 
     println("Имя папки: ${secretFolder.folderName}")
