e1b2c142d7fe8eb2735077b4b3cc23b1564eea77
svetlanaza <svetlanaz.dev@gmail.com>
2024-10-01 20:51:53 +0300

    KS-10-5-test

---
+++ src/main/kotlin/lesson_15/lesson15_task5.kt
@@ -0,0 +1,15 @@
+package lesson_15
+
+fun main() {
+    val numOfComponentsPerOnePortion = listOf(2, 55, 15)
+    println("Сколько порций омлета вы хотите приготовить?")
+    val numOfPortions = readln().toInt()
+
+    val neededComponents = numOfComponentsPerOnePortion.map { it * numOfPortions }
+    println(
+        "\nНа $numOfPortions порций вам понадобится: " +
+                "Яиц – ${neededComponents[0]} шт, " +
+                "молока – ${neededComponents[1]} мл, " +
+                "сливочного масла – ${neededComponents[2]} гр."
+    )
+}
